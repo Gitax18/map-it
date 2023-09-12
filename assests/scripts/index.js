96e0643c300259da853344080894a6b91df7905a
Gitanshu sankhla <lusifer0418@gmail.com>
2023-09-12 12:48:45 +0530

    restarting project

--- assests/scripts/index.js
+++
@@ -1,9 +0,0 @@
-const email = document.querySelector("#email").value;
-const password = document.querySelector("#password").value;
-const loginBtn = document.querySelector("#btn-submit");
-const linkRegister = document.querySelector("#link-register");
-
-linkRegister.addEventListener('click',(e)=>{
-    e.preventDefault()
-    window.location.href = "register.html";
-})
