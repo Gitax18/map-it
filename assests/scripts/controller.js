0c1ff8e7781d6182943652aadc2ec35f63c302b4
Gitanshu sankhla <lusifer0418@gmail.com>
2023-09-14 13:33:19 +0530

    .

---
+++ assests/scripts/controller.js
@@ -0,0 +1,24 @@
+// INPUTING IMAGE IN FORM *********************************
+// const input = document.querySelector('#select-image')
+// const image_holder = document.querySelector('.image')
+// let image = '';
+
+// console.log(input)
+// console.log(image_holder)
+
+// input.addEventListener('change', function(){
+//    const reader = new FileReader();
+//    reader.addEventListener('load', ()=>{
+//         image = reader.result;
+//         console.log(image)
+//         image_holder.style.backgroundImage = `url(${image})`;
+//    })
+
+//    reader.readAsDataURL(this.files[0]);
+// })
+
+class APP{
+    constructor() {
+        
+    }
+}
